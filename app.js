f39adb042f971a307cba7cc8f3d0322b30d94f55
Aditya bandaru <adityabandaruuu18@gmail.com>
2024-02-07 18:27:08 +0530

    Updated app.js and removed the bug

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
+let s = $("#movies");
+let s1 = $(".seats");
+let sc = $(".screen");
 
-let s=$("#movies");
-let s1=$(".seats");
-let sc=$(".screen");
-s1.click((event)=>{
-    let k= event.target;
-    $(k).toggleClass("selected");
-    let x=$(".selected").length;
-    $(".s1").text(x);
-    $(".s2").text(x*s.val());
-    
-})
+s1.click((event) => {
+    let k = event.target;
+    if ($(k).attr("id") !== "seatOc") { // Check if clicked seat is not the selected seat
+        $(k).toggleClass("selected");
+        let x = $(".selected").length;
+        $(".s1").text(x);
+        $(".s2").text(x * s.val());
+    }
+});
 
-s.change((e)=>{
+s.change((e) => {
     s1.removeClass("selected");
-    let x=$(".selected").length;
+    let x = $(".selected").length;
     $(".s1").text(x);
-    $(".s2").text(x*s.val());
-
-})
-
-
-// $(".seats").click((e)=>{
-//     let k=e.target;
-//     $(k).toggleClass("selected");
-//     let x=$(".selected").length;
-//     $(".s1").text(x);
-//     $(".s2").text(x*($("#movies").val()));
-    
-// })
-// $("#movies").change((e)=>{
-//     let k=e.target;
-//     $(k).removeClass("selected");
-//     let x=$(".selected").length;
-//     $(".s1").text(x);
-//      $(".s2").text(x*s.val());
-    
-
-// })
+    $(".s2").text(x * s.val());
+});
